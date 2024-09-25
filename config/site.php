5e9a8f2f6941248e548d9e99d7bf647bedd01eea
Sayee Deshmukh <sayyeee21@gmail.com>
2024-09-25 14:52:30 +0530

    SD61-sayeedeshmukh: Added config for social media links

---
+++ config/site.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'social' => [
+        'facebook' => 'https://facebook.com/your-profile',
+        'twitter' => 'https://twitter.com/your-profile',
+        'linkedin' => 'https://linkedin.com/in/your-profile',
+    ],
+];
