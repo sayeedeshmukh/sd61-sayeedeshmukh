e20d364b519d24dbfee7f21ce162c69dfb14e463
Sayee Deshmukh <sayyeee21@gmail.com>
2024-09-25 14:40:03 +0530

    SD61-sayeedeshmukh:Installed TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.blade.php',  // Scans all Blade files in your resources directory
+    './resources/**/*.js',         // Scans all JS files in your resources directory
+    './resources/**/*.vue',        // If you are using Vue.js
+    './public/**/*.html',          // Scans static HTML files if you have any
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
